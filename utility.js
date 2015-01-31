ca23627fd6698127ff7247b49f83306c43700c5b
Andrea Cresta <andrea@sixty.no>
2015-01-31 15:15:27 +0100

    logaritmic scale

--- utility.js
+++ utility.js
@@ -2,6 +2,8 @@ var coloursArray = ['#FFEDA0','#FED976','#FEB24C','#FD8D3C','#FC4E2A','#E31A1C',
 
 var rangeArray = [0,10,20,50,100,200,500,1000];
 
+    var logaritmicScale = false;
+
 // get color depending on population density value
 function getColor(d) 
 {
@@ -16,9 +18,19 @@ function getColor(d)
 
 function setMax(max) 
 {
-    var interval = max - 0;
-    var steps = interval / 8;
-    for (var i = rangeArray.length - 1; i >= 0; i--) {
-        rangeArray[i] = 0 + Math.ceil(i * steps);
-    };
+    if(logaritmicScale)
+    {
+        var steps = Math.pow(max, 1/8);
+
+        for (var i = rangeArray.length - 1; i >= 0; i--) {
+            rangeArray[i] = Math.ceil(i * steps);
+        };
+    }   
+    else
+    {
+        var steps = max / 8;
+        for (var i = rangeArray.length - 1; i >= 0; i--) {
+            rangeArray[i] = Math.ceil(i * steps);
+        };
+    }
 }

fb0e53f687386ed24c7081499b7be390c1f7d4f9
Torstein Thune <torstein@gmail.com>
2015-01-31 12:41:28 +0100

    Server interaction

---
+++ serverShait.js
@@ -0,0 +1,15 @@
+// Ruben's IP =D
+var serverUrl = "?callback=yay";
+
+var jqxhr = $.getJSON(serverUrl, function() {
+	console.log('GREAT SUCCESS!');
+})
+.done(function() {
+
+})
+.fail(function() {
+	console.log('HORRIBLE FAILURE =(');
+})
+.always(function() {
+
+});
